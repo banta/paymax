5cd46c49f5247bd5dc50524d36f2a140a62d939f
Bernard Banta <banta1@hotmail.com>
2012-01-14 18:27:10 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paymax::Application.config.secret_token = '87aaece04230850f3cd3c6fc67e7e444c56591995faf16f367b596e94272d9dcfa5aa9582dbcedb71183f479f3aeacbd172014717381cb519c481ce225cda433'
